5bb460a0ef1ea5628f09e74efe281655090fe0ac
Bilqis Zahra <145982762+zaraaa12@users.noreply.github.com>
2023-10-28 13:42:09 +0700

    Add files via upload

---
+++ Tugas 4 (Perulangan)/loop.java
@@ -0,0 +1,28 @@
+public class loop {
+    public static void main(String[] args) {
+        System.out.print("");
+
+        for (int i = 11; i >= -4; i-=3) {
+            System.out.print("" +i);
+
+        System.out.print("");
+            int c = 1;
+            for (int b =1; b<=6; b++){
+                System.out.print(c+"");
+                c = c + i;
+            
+            int a = 0;
+            for (int u = 0; u<=2; u++){
+                for (int l = 1; l<=5; l++){
+                    a = u+l;
+                    System.out.print(a+"");
+            System.out.print();
+                }
+            
+            }
+        }
+        }
+    }
+}
+
+
